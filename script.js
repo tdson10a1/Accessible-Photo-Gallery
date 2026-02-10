8e20cbc3c06ed1aa6b7dd1152c8c9a60134d2c1b
tdson10a1 <tdson10a1@gmail.com>
2026-02-10 21:32:44 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function upDate(previewPic) {
     const imageDiv = document.getElementById("image");
     imageDiv.style.backgroundImage = "url('" + previewPic.src + "')";
-    imageDiv.innerHTML = "";
+    imageDiv.textContent = "";
 }
 
 function unDo() {
     const imageDiv = document.getElementById("image");
     imageDiv.style.backgroundImage = "none";
-    imageDiv.innerHTML = "Hover over an image below to display here.";
+    imageDiv.textContent = "Hover over an image below to display here.";
 }
