ed8a5da29731483092e47c6a3796fff9a7eae967
tdson10a1 <tdson10a1@gmail.com>
2026-02-10 21:50:16 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 function upDate(previewPic) {
     const imageDiv = document.getElementById("image");
+
     imageDiv.style.backgroundImage = "url('" + previewPic.src + "')";
-    imageDiv.innerHTML = "";
+    imageDiv.textContent = previewPic.alt;
 }
 
 function unDo() {
     const imageDiv = document.getElementById("image");
+
     imageDiv.style.backgroundImage = "none";
-    imageDiv.innerHTML = "Hover over an image below to display here.";
+    imageDiv.textContent = "Hover over an image below to display here.";
 }
+
+window.onload = function () {
+    const images = document.querySelectorAll(".gallery img");
+
+    for (let i = 0; i < images.length; i++) {
+        images[i].setAttribute("tabindex", "0");
+    }
+
+    console.log("Tabindex added to all images");
+};
